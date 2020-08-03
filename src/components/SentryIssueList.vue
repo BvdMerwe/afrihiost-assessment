9dd11e1d4adcaa1c2e635ebdc370ba123861e081
Bernard vd Merwe <barny.vdm@gmail.com>
2020-08-03 10:43:55 +0200

    added headings to sentry

--- src/components/SentryIssueList.vue
+++ src/components/SentryIssueList.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="sentry-issue-list">
+        <div class="headings">
+            <span></span>
+            <span>Users affected</span>
+            <span>events</span>
+        </div>
         <SentryIssue v-for="issue in issues" :key="issue.index" :issue="issue"></SentryIssue>
     </div>
 </template>
@@ -17,10 +22,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .sentry-issue-list {
     display: grid;
     align-items: center;
     row-gap: 10px;
+
+    &:nth-child(1){
+        margin-top: -10px;
+    }
+}
+.headings {
+    font-size: 10px;
+    line-height: 10px;
+    font-weight: 600;
+    text-transform: uppercase;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr;
+    justify-items: start;
+    padding: 0 27px;
+    margin-top: -10px;
 }
 </style>
